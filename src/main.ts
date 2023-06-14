11c252310f87ea490d49e1f381eafe1cacc33c32
Dev <classifiedrealname@gmail.com>
2023-06-14 17:44:28 -0400

    portfolio

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import '@/assets/styles.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
+
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/toast';
+import 'bootstrap/js/dist/tooltip';
