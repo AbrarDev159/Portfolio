9203a2a09c8ed5f14410423750fe1ddab1fa470b
Dev <classifiedrealname@gmail.com>
2023-06-23 16:46:08 -0400

    ui change, added portfolio & todo project

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,21 @@
         <div class="navbar-nav mx-auto">
           <RouterLink class="nav-link" :class="{ 'active': $route.path === '/' }" aria-current="page" to="/">Home</RouterLink>
           <RouterLink class="nav-link" :class="{ 'active': $route.path === '/blog' }" to="/blog">Blog</RouterLink>
+          <a class="icon-link nav-link text-decoration-none" href="https://github.com/AbrarDev159/Todo">
+            Github
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="dark" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+              <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+            </svg>
+          </a>
         </div>
       </div>
     </div>
   </nav>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 m-2">
+  <div class="row row-cols-1 row-cols-lg-2 g-4 m-2">
     <div v-for="item in items" :key="item.id" class="col">
       <div class="card h-100">
         <!-- <img src="..." class="card-img-top" alt="..."> -->
@@ -14,8 +14,8 @@
           <h5 class="card-title">{{ item.name }}</h5>
           <p class="card-text">{{ item.technology }}</p>
           <p class="card-text">{{ item.description }}</p>
-          <p class="card-text"><a :href="item.github" target="_blank" rel="noopener noreferrer">{{ item.github }}</a></p>
-          <p class="card-text"><a :href="item.website" target="_blank" rel="noopener noreferrer">{{ item.website }}</a></p>
+          <p class="card-text">Github: <a :href="item.github" target="_blank" rel="noopener noreferrer">{{ item.github }}</a></p>
+          <p class="card-text">Website: <a :href="item.website" target="_blank" rel="noopener noreferrer">{{ item.website }}</a></p>
         </div>
       </div>
     </div>
